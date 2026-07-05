7711b5349785364242bdb6ef6220b19ba1ad64ca
Tiago Araujo <114108170+tiagofaraujo@users.noreply.github.com>
2026-07-05 18:33:32 +0100

    Refactor Navbar component to use Link from react-router

--- src/components/js/Navbar.js
+++ src/components/js/Navbar.js
@@ -1,43 +1,116 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
+
 import '../css/Navbar.css';
 
 function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <header className="navbar">
-            <div className="logo-container">
-                <a href="/">
-                    <img src="/logo.png" alt="CIRC Logo" className="logo" />
-                </a>
-            </div>
-            <nav className="main-nav">
-                <ul className="nav-links">
-                    <li><a href="/exhibition">EXHIBITION</a></li>
-                    <li><a href="/schedule">PROGRAMA</a></li>
-                    <li><a href="/sponsors">PARCERIAS</a></li>
-                    <li><a href="/hotels">HOTEL/REST</a></li>
-                    <li><a href="/contact">CONTACTOS</a></li>
-                </ul>
-                {/* Este botão só vai aparecer na versão desktop */}
-                <a href="https://form.jotform.com/242143179037353" className="primary-btn desktop-only">
-                    Inscrição
-                </a>
-            </nav>
-            <div className="hamburger-menu">
-                <Menu right isOpen={isOpen} onStateChange={({ isOpen }) => setIsOpen(isOpen)}>
-                    <a className="menu-item" href="/">Início</a>
-                    <a className="menu-item" href="/exhibition">Exhibition</a>
-                    <a className="menu-item" href="/schedule">Programa</a>
-                    <a className="menu-item" href="/sponsors">Parcerias</a>
-                    <a className="menu-item" href="/hotels">Hotel/Rest</a>
-                    <a className="menu-item" href="/contact">Contactos</a>
-                    {/* Não incluir o botão de inscrição aqui no menu hamburger */}
-                </Menu>
-            </div>
-        </header>
-    );
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <header className="navbar">
+      <div className="logo-container">
+        <Link to="/" onClick={closeMenu}>
+          <img
+            src="/logo.png"
+            alt="CIRC — Congresso Internacional de Radiologia de Coimbra"
+            className="logo"
+          />
+        </Link>
+      </div>
+
+      <nav className="main-nav">
+        <ul className="nav-links">
+          <li>
+            <Link to="/exhibition">EXHIBITION</Link>
+          </li>
+
+          <li>
+            <Link to="/schedule">PROGRAMA</Link>
+          </li>
+
+          <li>
+            <Link to="/sponsors">PARCERIAS</Link>
+          </li>
+
+          <li>
+            <Link to="/hotels">HOTEL/REST</Link>
+          </li>
+
+          <li>
+            <Link to="/contact">CONTACTOS</Link>
+          </li>
+        </ul>
+
+        <a
+          href="https://form.jotform.com/242143179037353"
+          className="primary-btn desktop-only"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Inscrição
+        </a>
+      </nav>
+
+      <div className="hamburger-menu">
+        <Menu
+          right
+          isOpen={isOpen}
+          onStateChange={({ isOpen: menuIsOpen }) =>
+            setIsOpen(menuIsOpen)
+          }
+        >
+          <Link className="menu-item" to="/" onClick={closeMenu}>
+            Início
+          </Link>
+
+          <Link
+            className="menu-item"
+            to="/exhibition"
+            onClick={closeMenu}
+          >
+            Exhibition
+          </Link>
+
+          <Link
+            className="menu-item"
+            to="/schedule"
+            onClick={closeMenu}
+          >
+            Programa
+          </Link>
+
+          <Link
+            className="menu-item"
+            to="/sponsors"
+            onClick={closeMenu}
+          >
+            Parcerias
+          </Link>
+
+          <Link
+            className="menu-item"
+            to="/hotels"
+            onClick={closeMenu}
+          >
+            Hotel/Rest
+          </Link>
+
+          <Link
+            className="menu-item"
+            to="/contact"
+            onClick={closeMenu}
+          >
+            Contactos
+          </Link>
+        </Menu>
+      </div>
+    </header>
+  );
 }
 
 export default Navbar;
