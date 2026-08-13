aec92745beaed330f9abf8f1cc2702e6567305e4
Tiago Araujo <114108170+tiagofaraujo@users.noreply.github.com>
2026-08-13 02:34:09 +0100

    Corrigir prioridade das fontes Barlow e Alike Angular

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+// CSS de bibliotecas primeiro. A identidade CIRC é carregada depois para ter prioridade
+// sobre os estilos tipográficos por defeito do Bootstrap/MDB.
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
