eed324f4c59dc34fde74dc7cd00576c3a382551f
Tiago Araujo <114108170+tiagofaraujo@users.noreply.github.com>
2026-08-29 21:16:42 +0100

    Replace republic logo with Government of Portugal

--- src/components/js/Footer.js
+++ src/components/js/Footer.js
@@ -103,7 +103,7 @@ function Footer() {
             rel="noopener noreferrer"
             aria-label={en ? 'Portuguese Government website' : 'Website do Governo de Portugal'}
           >
-            <img src="/republica-portuguesa-logo-negative.svg" alt="República Portuguesa" />
+            <img src="/governo-portugal-logo.webp" alt="Governo de Portugal" />
           </a>
           <a
             className="site-footer__support-logo site-footer__support-logo--sns"
